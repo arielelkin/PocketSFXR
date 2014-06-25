1d099a01248d9a5074b01f284d21b9fb7a9eb699
Ariel Elkin <arielelkin@gmail.com>
2014-06-25 01:30:02 -0700

    Initial Commit

---
+++ PocketSFXR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PocketSFXR
+//
+//  Created by Ariel Elkin on 25/06/2014.
+//  Copyright (c) 2014 Ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PocketSFXRTests/PocketSFXRTests.m
@@ -0,0 +1,34 @@
+//
+//  PocketSFXRTests.m
+//  PocketSFXRTests
+//
+//  Created by Ariel Elkin on 25/06/2014.
+//  Copyright (c) 2014 Ariel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PocketSFXRTests : XCTestCase
+
+@end
+
+@implementation PocketSFXRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
